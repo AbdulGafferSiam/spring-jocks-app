b209cf5fa85b8f4ee04cbe4a2774ebd5c54df5b9
AbdulGafferSiam <abdulgaffersiam@outlook.com>
2023-06-07 14:14:56 +0900

    initial commit - create spring boot project

---
+++ src/main/java/me/personalproject/springjocksapp/SpringJocksAppApplication.java
@@ -0,0 +1,13 @@
+package me.personalproject.springjocksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJocksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJocksAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/personalproject/springjocksapp/SpringJocksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.personalproject.springjocksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJocksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
